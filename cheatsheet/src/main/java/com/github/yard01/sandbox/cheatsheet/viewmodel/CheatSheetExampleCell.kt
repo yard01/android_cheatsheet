e23e4f42bbe2f1bc9d43a8510336319b375efc4d
yard <yardmitry@gmail.com>
2020-03-01 01:11:44 +0300

    Example Bridge

--- cheatsheet/src/main/java/com/github/yard01/sandbox/cheatsheet/viewmodel/CheatSheetExampleCell.kt
+++ cheatsheet/src/main/java/com/github/yard01/sandbox/cheatsheet/viewmodel/CheatSheetExampleCell.kt
@@ -2,14 +2,15 @@ package com.github.yard01.sandbox.cheatsheet.viewmodel
 
 import android.graphics.Bitmap
 import com.github.yard01.sandbox.cheatsheet.ExampleBridge
+import java.util.function.Supplier
 
 class CheatSheetExampleCell(var bridge: ExampleBridge) {
     var info = ""
     var bitmap: Bitmap? = null
-    val iconProvider = bridge::getIcon
+    //val iconProvider: KFunction = bridge::getIcon
     init {
 
-
+        //iconProvider.getIcon()
         //var iconProvider: IconProvider = { context: Context -> bridge.loadIcon(context) }
         //iconPovider = { context: Context -> bridge.loadIcon(context) }
         //iconPovider.
