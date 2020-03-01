07c5dcd436d07ec27a0dbc59f7301fdc46bb5d0f
yard <yardmitry@gmail.com>
2020-03-01 20:56:50 +0300

    Run Bridge Application

--- app/src/main/java/com/github/yard01/androidcheatsheet/ui/ExamplePageFragment.kt
+++ app/src/main/java/com/github/yard01/androidcheatsheet/ui/ExamplePageFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.github.yard01.androidcheatsheet.R
 import com.github.yard01.sandbox.cheatsheet.viewmodel.CheatSheetExampleCell
@@ -20,7 +21,8 @@ class ExamplePageFragment(val cell: CheatSheetExampleCell): Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val result: View = inflater.inflate(R.layout.example_page_fragment, container, false)
-        result.setOnClickListener { view -> clickRun(view) }
+        val runButton: Button = result.findViewById(R.id.run_example_Button)
+        runButton.setOnClickListener { view -> clickRun(view) }
         return result
     }
 }
