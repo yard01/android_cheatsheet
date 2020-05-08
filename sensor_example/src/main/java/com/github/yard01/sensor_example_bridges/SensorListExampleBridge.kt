ad920c92fa763633fbadd85b6006f71a3f65cf52
yard <yardmitry@gmail.com>
2020-05-09 00:58:42 +0300

    sensor list screenshot

--- sensor_example/src/main/java/com/github/yard01/sensor_example_bridges/SensorListExampleBridge.kt
+++ sensor_example/src/main/java/com/github/yard01/sensor_example_bridges/SensorListExampleBridge.kt
@@ -37,12 +37,11 @@ class SensorListExampleBridge (var context: Context): ExampleBridge, IconProvide
 
     override fun getScreenshotCount(): Int {
      //   TODO("Not yet implemented")
-        return 0
+        return 1
     }
 
     override fun getScreenshotId(index: Int): Int {
-    //    TODO("Not yet implemented")
-        return -1
+        return R.drawable.sensorlist_screenshot_0
     }
 
 }
