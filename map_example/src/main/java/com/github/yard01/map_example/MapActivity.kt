a4baa21608dba23ca5d9c0f1d8e765fd26ea604f
yard01 <yardmitry@gmail.com>
2020-09-17 17:54:23 +0300

    Map Fragment

--- map_example/src/main/java/com/github/yard01/map_example/MapActivity.kt
+++ map_example/src/main/java/com/github/yard01/map_example/MapActivity.kt
@@ -1,14 +1,19 @@
 package com.github.yard01.map_example
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     companion object {
         val FRAGMENT_CLASSNAME = "fragment_classname"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.map_layout)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        //TODO("Not yet implemented")
     }
 }
