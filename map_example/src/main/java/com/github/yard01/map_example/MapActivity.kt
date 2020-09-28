81583d23e0558296260b81b36962e8e4b13ed745
yard <yad@bk.ru>
2020-09-29 01:33:25 +0300

    Map Fragment Example + Australia marker

--- map_example/src/main/java/com/github/yard01/map_example/MapActivity.kt
+++ map_example/src/main/java/com/github/yard01/map_example/MapActivity.kt
@@ -1,11 +1,17 @@
 package com.github.yard01.map_example
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.map_layout.*
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
     companion object {
         val FRAGMENT_CLASSNAME = "fragment_classname"
     }
@@ -14,10 +20,18 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.map_layout)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        this.showLocationButton.setOnClickListener {
+            val sydney = LatLng(-34.0, 151.0)
+            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
 
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
-        //TODO("Not yet implemented")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        
     }
 }
