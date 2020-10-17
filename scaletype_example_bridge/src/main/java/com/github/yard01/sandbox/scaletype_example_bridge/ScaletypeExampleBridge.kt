9fda4c08225b6302ea5bcd033e966cc62c8199a4
yard <yardmitry@gmail.com>
2020-10-18 00:06:44 +0300

    ImageView : matrix

--- scaletype_example_bridge/src/main/java/com/github/yard01/sandbox/scaletype_example_bridge/ScaletypeExampleBridge.kt
+++ scaletype_example_bridge/src/main/java/com/github/yard01/sandbox/scaletype_example_bridge/ScaletypeExampleBridge.kt
@@ -1,13 +1,19 @@
 package com.github.yard01.sandbox.scaletype_example_bridge
 
 import android.content.Context
+import android.content.Intent
+import com.example.matrixscale.EntryActivity
+import com.example.matrixscale.MainActivity
 import com.github.yard01.sandbox.cheatsheet.ExampleBridge
 import com.github.yard01.sandbox.cheatsheet.viewmodel.IconProvider
 import com.github.yard01.sandbox.cheatsheet.viewmodel.ScreenshotProvider
 
 class ScaletypeExampleBridge (var context: Context): ExampleBridge, IconProvider, ScreenshotProvider {
     override fun executeExample() {
-
+        val intent = Intent(context, EntryActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        context.startActivity(intent)
     }
 
     override fun getIconId(): Int = -1
